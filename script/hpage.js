68410a45b484a1d9a99332ad6788e6d89b7347fb
Gloryjaw <dkushwah9@email.com>
2023-11-26 15:34:18 +0530

    Added homepage

---
+++ script/hpage.js
@@ -0,0 +1,33 @@
+ham = document.querySelector("#hamburger_button")
+nav = document.querySelector(".header__nav-section")
+
+ham.addEventListener("click", function(){
+    let dsp = window.getComputedStyle(nav).display
+    if(dsp == "none"){
+        nav.style.display = "block"
+    }
+
+})
+document.addEventListener("click", function(e){
+    if(e.target != ham){
+        let turnoff = true;
+        let path = e.composedPath()
+
+        for(let i of path){
+            if(i.classList ){
+                if(!i.classList.contains("header__nav-section")){
+                    turnoff = true;
+                }
+                else{
+                    turnoff = false;
+                    break;
+                }
+            }
+    
+        }
+        if(turnoff){
+            nav.style.display = "none"
+        }
+    }
+
+})
